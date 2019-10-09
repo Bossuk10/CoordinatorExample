8253b3ffe843cf21128cc20306f71b93fd5aec36
Mike Shtryhel <m.shtryhel@mytaxi.com>
2019-10-09 17:30:58 +0200

    Initial Commit

---
+++ CoordinatorsDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoordinatorsDemo
+//
+//  Created by Mykhailo Shtryhel on 09.10.19.
+//  Copyright © 2019 Mykhailo Shtryhel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
